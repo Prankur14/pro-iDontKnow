ffdcab79978791eb959dc68ac07faf6b17512e0f
Prankur14 <70706874+Prankur14@users.noreply.github.com>
2021-09-04 18:10:34 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,38 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+
+    fill("white")
+    textSize(25)
+    textFont('eras itc')
+    text("R E V B A I L A", 100, 50)
+    fill("lightblue")
+    push();
+    textSize(15)
+    text("Hint: Always changing, not constant !!", 100, 70)
+    pop();
+    fill("white")
+    textSize(25)
+    text("C U T N I F O N", 700, 150)
+    fill("lightblue")
+
+    push()
+    textSize(15);
+    text("Hint: Performs a particular task !!", 700, 170)
+    pop();
+    fill("white")
+    push()
+    textSize(25)
+    text("A T E D A S B A", 100, 250);
+    pop();
+    fill("lightblue");
+
+
+    push();
+    textSize(15);
+    text("Hint: Stores all information !!", 100, 270)
+    pop();
+
+}
